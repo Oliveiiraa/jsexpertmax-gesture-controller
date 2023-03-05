abe4a839b74009d484ce23ab830055ea53651222
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2023-03-05 18:57:10 -0300

    feat: add support for firefox

--- classes/class01/pages/video-player/src/factory.js
+++ classes/class01/pages/video-player/src/factory.js
@@ -10,14 +10,44 @@ async function getWorker() {
     const worker = new Worker("./src/worker.js", { type: "module" });
     return worker;
   }
+
   console.warn(`Your browser doesn't support esm modules on webworkers!`);
+  console.warn(`Importing libraries`);
+
+  await import("https://unpkg.com/@tensorflow/tfjs-core@2.4.0/dist/tf-core.js");
+  await import(
+    "https://unpkg.com/@tensorflow/tfjs-converter@2.4.0/dist/tf-converter.js"
+  );
+  await import(
+    "https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.4.0/dist/tf-backend-webgl.js"
+  );
+  await import(
+    "https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist/face-landmarks-detection.js"
+  );
+
+  console.warn(`using worker mock insted!`);
+
+  const service = new Service({
+    faceLandmarksDetection: window.faceLandmarksDetection,
+  });
 
   const workerMock = {
-    async postMessage() {},
+    async postMessage(video) {
+      const blinked = await service.handBlinked(video);
+      if (!blinked) return;
+      workerMock.onmessage({ data: { blinked } });
+    },
     onmessage(msg) {},
   };
 
-  console.log("não suporta");
+  await service.loadModel();
+
+  console.log("TF model loaded");
+
+  setTimeout(() => {
+    worker.onmessage({ data: "READY" });
+  }, 500);
+
   return workerMock;
 }
 
